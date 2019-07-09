e1369f23d6a688314a478bec22b698b25667cdac
buybackoff <vbaybekov@gmail.com>
2019-07-09 14:00:03 +0300

    Resurrect

--- js/scripts.js
+++ js/scripts.js
@@ -18,3 +18,13 @@ jQuery(function($) {
 });
 
 hljs.initHighlightingOnLoad();
+hljs.initLineNumbersOnLoad();
+tocbot.init({
+    // Where to render the table of contents.
+    tocSelector: '.toc',
+    // Where to grab the headings to build the table of contents.
+    contentSelector: '.post-container',
+    // Which headings to grab inside of the contentSelector element.
+    headingSelector: 'h1, h2, h3',
+    collapseDepth: 3
+  });
