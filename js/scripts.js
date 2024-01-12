09cc3fe27dc28fb05f182988cfb88d189780e7ea
Victor Baybekov <vbaybekov@gmail.com>
2024-01-12 10:03:19 +0100

    Update

--- js/scripts.js
+++ js/scripts.js
@@ -1,30 +1,52 @@
-jQuery(function($) {
+// @ts-check
 
-    /* ============================================================ */
-    /* Responsive Videos */
-    /* ============================================================ */
+jQuery(function ($) {
+  /* ============================================================ */
+  /* Responsive Videos */
+  /* ============================================================ */
 
-    $(".post-content").fitVids();
+  $(".post-content").fitVids();
 
-    /* ============================================================ */
-    /* Scroll To Top */
-    /* ============================================================ */
+  /* ============================================================ */
+  /* Scroll To Top */
+  /* ============================================================ */
 
-    $('.js-jump-top').on('click', function(e) {
-        e.preventDefault();
+  $(".js-jump-top").on("click", function (e) {
+    e.preventDefault();
 
-        $('html, body').animate({'scrollTop': 0});
+    $("html, body").animate({ scrollTop: 0 });
+  });
+
+  // Tabs
+
+  $(".xtabs").each(function () {
+    var t = $(this.childNodes);
+
+    var links = t.find("li a");
+    links.on("click", function () {
+      var id = $(this).attr("id");
+      if ($(this).hasClass("inactive")) {
+        links.addClass("inactive");
+        $(this).removeClass("inactive");
+
+        t.filter(".xtabscontainer").hide();
+        t.filter("#" + id + "C").fadeIn(0);
+      }
     });
+
+    t.find("li a:not(:first)").addClass("inactive");
+    t.filter(".xtabscontainer").hide();
+    t.filter(".xtabscontainer:first").show();
+    t.find("a").filter(".xtabselected").trigger("click");
+  });
 });
 
-hljs.initHighlightingOnLoad();
-hljs.initLineNumbersOnLoad();
 tocbot.init({
-    // Where to render the table of contents.
-    tocSelector: '.toc',
-    // Where to grab the headings to build the table of contents.
-    contentSelector: '.post-container',
-    // Which headings to grab inside of the contentSelector element.
-    headingSelector: 'h1, h2, h3',
-    collapseDepth: 4
-  });
+  // Where to render the table of contents.
+  tocSelector: ".toc",
+  // Where to grab the headings to build the table of contents.
+  contentSelector: ".post-container",
+  // Which headings to grab inside of the contentSelector element.
+  headingSelector: "h1, h2, h3",
+  collapseDepth: 4,
+});
