b0d0c383887565097ad371ca9a26e6e19c5e9a2a
Victor Baybekov <vbaybekov@gmail.com>
2024-01-17 22:41:36 +0100

    Update results, TOC on mobile

--- js/scripts.js
+++ js/scripts.js
@@ -39,6 +39,12 @@ jQuery(function ($) {
     t.filter(".xtabscontainer:first").show();
     t.find("a").filter(".xtabselected").trigger("click");
   });
+
+  $(document).on("pagecreate", "#pageone", function () {
+    $("p").on("swiperight", function () {
+      alert("You swiped right!");
+    });
+  });
 });
 
 tocbot.init({
@@ -48,5 +54,63 @@ tocbot.init({
   contentSelector: ".post-container",
   // Which headings to grab inside of the contentSelector element.
   headingSelector: "h1, h2, h3",
-  collapseDepth: 4,
+  collapseDepth: 3,
+});
+
+function hasClass(ele, cls) {
+  return !!ele.className.match(new RegExp("(\\s|^)" + cls + "(\\s|$)"));
+}
+
+function addClass(ele, cls) {
+  if (!hasClass(ele, cls)) ele.className += " " + cls;
+}
+
+function removeClass(ele, cls) {
+  if (hasClass(ele, cls)) {
+    var reg = new RegExp("(\\s|^)" + cls + "(\\s|$)");
+    ele.className = ele.className.replace(reg, " ");
+  }
+}
+
+//Add event from js the keep the marup clean
+function init() {
+  document.getElementById("toc").addEventListener("click", toggleMenu);
+  document.getElementById("toc-button").addEventListener("click", toggleMenu);
+  document.getElementById("body-overlay").addEventListener("click", toggleMenu);
+}
+
+function isSideBar() {
+  var button = document.getElementById("toc-button");
+  return window.getComputedStyle(button).display != "none";
+}
+
+//The actual fuction
+function toggleMenu() {
+  var ele = document.getElementsByTagName("body")[0];
+  if (isSideBar() && !hasClass(ele, "toc-open")) {
+    addClass(ele, "toc-open");
+  } else {
+    removeClass(ele, "toc-open");
+  }
+}
+
+window.addEventListener("scroll", function () {
+  const fixedElement = document.getElementById("toc");
+  if (isSideBar()) {
+    fixedElement.style.top = "0";
+    return;
+  }
+  
+  if (window.scrollY > 80) {
+    fixedElement.style.top = "10px";
+  } else {
+    fixedElement.style.top = Math.max(10, 80 - window.scrollY) + "px";
+  }
+});
+
+//Prevent the function to run before the document is loaded
+document.addEventListener("readystatechange", function () {
+  if (document.readyState === "complete") {
+    init();
+  }
 });
